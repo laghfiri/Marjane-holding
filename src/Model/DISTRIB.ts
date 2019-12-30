2aff722e0fc43638171f26582c29df7a9566ef23
unknown <fatimazahra.laghfiri@gmail.com>
2019-12-30 17:57:55 +0100

    Marjane

---
+++ src/Model/DISTRIB.ts
@@ -0,0 +1,15 @@
+export class DISTRIB{
+
+  public distribid:number;
+  public distribcod: string;
+  public distriblib: string;
+  public distribrem:string;
+  public discreated:Date =new Date();
+  public disupdatby:string;
+  public disupdated:Date=new Date();
+
+
+  constructor(){
+
+  }
+}
---
+++ src/Model/Supplier.ts
@@ -0,0 +1,44 @@
+
+export class FOURN {
+
+ public fournid :number=null;
+ public fourlib:string= "fournisseur libele";
+ public fournad:string= "foun adresse";
+ public fourcon :string = "CON ";
+ public foucreated :string =  "2019-02-15T00:00:00.000+0000";
+ public fourtypsociete:string= "erte";
+ public fouridentifste:string= "fournisersite";
+ public fourpurgeburord:number= 4;
+ public fourpurgestatcmd: number=  5;
+ public fourcod:string= "code fournisseur";
+ public fouupdatby: string= "lmlm";
+ public fouupdated: string = "2019-02-21T00:00:00.000+0000";
+ public foucomment: string= "comment";
+ public note: string= "JE NOTE QUE JE DOIS ETRE A LA FOIS JE SUIS DANS UN FICHIER";
+ //addresse
+ public commandea:string= "com ahmed tijani";
+ public reglera:string= "regle Ahmed";
+ public livrerpar:string= "Livrer ";
+ public facturerpar:string= "facture 01-0588";
+ //addresse
+ public logo:string= "Logo";
+ public fournumtel:string= "062138409080";
+ public fouremail:string= "email@email.com";
+ public fourrib:string= "04589";
+ public fourcapital:string= "10555800";
+ public fourabonn:number= 44;
+ public fouretat:number= 1;
+ public fourdatabon:Date=null;
+ public  fourpurgevent:number= 5;
+ public  fourpurgecmd:number= 5;
+ public  fourpurgerec:number= 5;
+ public  fourpurgereg: number= 7;
+ public  fourpurgerfa: number=8;
+ public  fourddebprix: Date=null;
+ public  fourvaltx: number=89;
+
+  constructor() {
+  }
+
+
+}
